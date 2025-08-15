1b7920ca37272b881ffa8b8aa97a1b993580e80d
Vivek Dhengale <vivekdhengale84@gmail.com>
2025-08-15 16:08:10 +0530

    added a new url and view for february challenge

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+months = {}
+def index(request):
+    return HttpResponse("This Works!")
+
+def feb(request):
+    return HttpResponse("This is Feb challenge")
