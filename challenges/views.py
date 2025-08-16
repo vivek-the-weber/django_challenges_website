cad526052e453992df8418d52f92ab0b1c26057b
Vivek Dhengale <vivekdhengale84@gmail.com>
2025-08-16 21:18:41 +0530

    added new template files and made a lot of changes in those making the website dynamic in nature

--- challenges/views.py
+++ challenges/views.py
@@ -15,16 +15,14 @@ months_challenges = {
     "sep" : "Step on november", 
     "oct":"opt for volunteering",
     "nov" : "Go to the Chember",
-    "dec": "Decide your goals for next year"
+    "dec": None,
      }
 
 def index(request):
     list_of_months_response = list(months_challenges.keys())
-    response = ""
-    for month_redirect in list_of_months_response:
-        reversed_path = reverse("month-challenge", args = [month_redirect]) # e.g. /challenge/jan
-        response += f"<a href ='{reversed_path}'>{month_redirect}</a><br>"
-    return HttpResponse(response)
+    return render(request,"challenges/index.html", {
+        "months" : list_of_months_response,
+    })
 def monthly_challenge_by_number(request, month):
     if month <= 12:
         months = list(months_challenges.keys())
@@ -34,9 +32,12 @@ def monthly_challenge_by_number(request, month):
     else:
         return HttpResponseNotFound("INVALID MONTH!!")
 def monthly_challenge(request, month):
+
     try:
-        challenges_text = months_challenges[month]
+        challenge_text = months_challenges[month]
+        return render(request, "challenges/challenge.html", {
+            "text" : challenge_text, "month_name" : month,
+        })
     except:
         return HttpResponseNotFound("Not a VALID FORMAT OF THE MONTH, enter jan, feb, etc...!!!")
-    return HttpResponse(challenges_text)
 
--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # BASE_DIR / "challenges" / "templates"
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
